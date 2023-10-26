649bb9325f83469b1b22fbbb285c36cf86b3458a
Justin Carrier <2162774@csfoy.ca>
2023-10-26 12:32:32 -0400

    Ajout de css - Index

---
+++ public/liaisons/js/main.js
@@ -0,0 +1,9 @@
+let categories = document.querySelectorAll(".item-categorie");
+
+for (i = 0; i < categories.length; i++) {
+    let stringCategorie = categories[i].innerText;
+    categories[i].innerHTML = stringCategorie.charAt(0).toUpperCase() + stringCategorie.slice(1);
+
+}
+
+
