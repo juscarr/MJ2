2aeb3093c81fc297790f7ac0b925734dfab0d321
Maxime Graignon <2031817@csfoy.ca>
2023-10-30 15:51:23 -0400

    Visionneuse fonctionnel en version mobile et table

--- public/liaisons/js/visionneuse.js
+++ public/liaisons/js/visionneuse.js
@@ -1,35 +1,155 @@
+const flecheDroite1 = document.querySelector('.cercleDroit1');
+const flecheGauche1 = document.querySelector('.cercleGauche1');
 
-const flecheDroite = document.querySelector('.cercleDroit1');
-const flecheGauche = document.querySelector('.cercleGauche1');
+const flecheDroite2 = document.querySelector('.cercleDroit2');
+const flecheGauche2 = document.querySelector('.cercleGauche2');
+
+const flecheDroite3 = document.querySelector('.cercleDroit3');
+const flecheGauche3 = document.querySelector('.cercleGauche3');
+
+
 const listeLivre = document.querySelectorAll(".item_li");
+const listeLivre2 = document.querySelectorAll(".item_li2");
+const listeLivre3 = document.querySelectorAll(".item_li3");
 
+
+let large = null;
 let index = 1;
+setInterval(verifierLargeur, 1000)
 
+function verifierLargeur() {
+    let largeurEcran = window.innerWidth;
+
+    if (largeurEcran >= 600) {
+        large = true;
+        listeLivre[index - 1].hidden = false;
+        listeLivre[index].hidden = false;
+        listeLivre[index + 1].hidden = false;
+
+    } else {
+        large = false;
+        listeLivre[index - 1].hidden = true;
+        listeLivre[index + 1].hidden = true;
+        listeLivre[index].hidden = false;
+
+    }
+}
+
+flecheDroite1.addEventListener('click', changerImageSuivant);
+flecheGauche1.addEventListener('click', changerImagePrecedent);
+
+/*****************************************************************/
 changerImageSuivant();
+// changerImageSuivant3();
 
-flecheDroite.addEventListener('click', changerImageSuivant);
-
-flecheGauche.addEventListener('click', changerImagePrecedent);
+/*****************************************************************/
 
 
 function changerImageSuivant() {
+    if (large) {
+        console.log(large)
+        listeLivre[index - 1].hidden = true;
+        listeLivre[index].hidden = true;
+        listeLivre[index + 1].hidden = true;
+        index++
+        listeLivre[index - 1].hidden = false;
+        listeLivre[index].hidden = false;
+        listeLivre[index + 1].hidden = false;
 
-    listeLivre[index].hidden = true;
-    index++;
-    listeLivre[index].hidden = false;
-    flecheGauche.classList.remove('flecheNonFonctionnel');
-    flecheGauche.addEventListener('click', changerImagePrecedent);
-    console.log(index)
+        flecheGauche1.classList.remove('flecheNonFonctionnel');
+        flecheGauche1.addEventListener('click', changerImagePrecedent);
+
+    } else {
+        listeLivre[index].hidden = true;
+        index++;
+        listeLivre[index].hidden = false;
+        flecheGauche1.classList.remove('flecheNonFonctionnel');
+        flecheGauche1.addEventListener('click', changerImagePrecedent);
+    }
 }
 
 function changerImagePrecedent() {
-    listeLivre[index].hidden = true
-    index--;
-    listeLivre[index].hidden = false;
+    if (large) {
+        listeLivre[index - 1].hidden = true;
+        listeLivre[index].hidden = true;
+        listeLivre[index + 1].hidden = true;
+        index--;
+        listeLivre[index - 1].hidden = false;
+        listeLivre[index].hidden = false;
+        listeLivre[index + 1].hidden = false;
 
-    if (index === 0){
-        flecheGauche.removeEventListener('click', changerImagePrecedent);
-        flecheGauche.classList.add('flecheNonFonctionnel');
+        console.log(index);
+
+        if (index === 1){
+            flecheGauche1.classList.add('flecheNonFonctionnel');
+            flecheGauche1.removeEventListener('click', changerImagePrecedent);
+        }
+
+    } else {
+        listeLivre[index].hidden = true;
+        index--;
+        listeLivre[index].hidden = false;
+
+
+        if (index === 0){
+            flecheGauche1.classList.add('flecheNonFonctionnel');
+            flecheGauche1.removeEventListener('click', changerImagePrecedent);
+        }
+
+    }
+
+
+}
+
+/*****************************************************************/
+flecheDroite2.addEventListener('click', changerImageSuivant2);
+flecheGauche2.addEventListener('click', changerImagePrecedent2);
+
+function changerImageSuivant2() {
+
+    listeLivre2[index].hidden = true;
+    index++;
+    listeLivre2[index].hidden = false;
+    flecheGauche2.classList.remove('flecheNonFonctionnel');
+    flecheGauche2.addEventListener('click', changerImagePrecedent2);
+    console.log(index)
+}
+
+function changerImagePrecedent2() {
+    listeLivre2[index].hidden = true
+    index--;
+    listeLivre2[index].hidden = false;
+
+    if (index === 0) {
+        flecheGauche2.removeEventListener('click', changerImagePrecedent2);
+        flecheGauche2.classList.add('flecheNonFonctionnel');
+        console.log('index est a 0');
+    }
+
+}
+
+/*****************************************************************/
+flecheDroite3.addEventListener('click', changerImageSuivant3);
+flecheGauche3.addEventListener('click', changerImagePrecedent3);
+
+function changerImageSuivant3() {
+
+    listeLivre3[index].hidden = true;
+    index++;
+    listeLivre3[index].hidden = false;
+    flecheGauche3.classList.remove('flecheNonFonctionnel');
+    flecheGauche3.addEventListener('click', changerImagePrecedent3);
+    console.log(index)
+}
+
+function changerImagePrecedent3() {
+    listeLivre3[index].hidden = true
+    index--;
+    listeLivre3[index].hidden = false;
+
+    if (index === 0) {
+        flecheGauche3.removeEventListener('click', changerImagePrecedent3);
+        flecheGauche3.classList.add('flecheNonFonctionnel');
         console.log('index est a 0');
     }
 
--- ressources/vues/accueil.blade.php
+++ ressources/vues/accueil.blade.php
@@ -18,8 +18,68 @@
     <div class="nouveautes">
         <!-- Importer les infos de la base de donnée en mode aléatoire (Titre de livre / Nom auteur / Catégorie / Prix en cad) -->
         <ul class="itemCacheEnMobile">
+
+
             @foreach($livres as $livre)
                 <li class="item_li" hidden>
+                    <img class="item_image" src="../images/img_couvert_livres/{{$livre->getCategorieId()}}/{{$livre->getIsbnPapier()}}.jpg" alt="">
+                    <h3 class="h3">{{$livre->getTitre()}}</h3>
+                    <p>@foreach ($livre->getAuteurAssociee($livre->getId()) as $livreAssocAuteur)
+                            {{$livreAssocAuteur->getAuteurAssoc($livreAssocAuteur->getIdAuteur())->getPrenom() . " " . $livreAssocAuteur->getAuteurAssoc($livreAssocAuteur->getIdAuteur())->getNom()}}
+                        @endforeach
+                    </p>
+                    <p>{{$livre->getCategorieAssociee($livre->getCategorieId())->getNom()}}</p>
+                    <p class="prix">{{$livre->getPrixCan()}}</p>
+                </li>
+            @endforeach
+
+        </ul>
+    </div>
+    <div class="flecheVisionneuse">
+        <div class="cercleGauche1">
+            <div class="flecheGauche"></div>
+        </div>
+        <div class="cercleDroit1">
+            <div class="flecheDroite"></div>
+        </div>
+    </div>
+
+    <h2 class="h2 h2_section2">Ceux qui ont reçu des prix</h2>
+    <div class="prix_recu">
+        <!-- Importer les infos de la base de donnée en mode aléatoire (Titre de livre / Nom auteur / Catégorie / Prix en cad) -->
+
+        <ul class="itemCacheEnMobile">
+            @foreach($livres as $livre)
+                <li class="item_li2" hidden>
+                    <img class="item_image" src="../images/img_couvert_livres/{{$livre->getCategorieId()}}/{{$livre->getIsbnPapier()}}.jpg" alt="">
+                    <h3>{{$livre->getTitre()}}</h3>
+                    <p>@foreach ($livre->getAuteurAssociee($livre->getId()) as $livreAssocAuteur)
+                            {{$livreAssocAuteur->getAuteurAssoc($livreAssocAuteur->getIdAuteur())->getPrenom() . " " . $livreAssocAuteur->getAuteurAssoc($livreAssocAuteur->getIdAuteur())->getNom()}}
+                        @endforeach</p>
+                    <p>{{$livre->getCategorieAssociee($livre->getCategorieId())->getNom()}}</p>
+                    <p class="prix">{{$livre->getPrixCan()}}</p>
+                </li>
+            @endforeach
+        </ul>
+
+        <div class="flecheVisionneuse">
+            <div class="cercleGauche2">
+                <div class="flecheGauche"></div>
+            </div>
+            <div class="cercleDroit2">
+                <div class="flecheDroite"></div>
+            </div>
+        </div>
+    </div>
+
+    <h2 class="h2 h2_section3">Des livres recommandés</h2>
+    <div class="livres_recommandes">
+
+        <!-- Importer les infos de la base de donnée en mode aléatoire (Titre de livre / Nom auteur / Catégorie / Prix en cad) -->
+
+        <ul class="itemCacheEnMobile">
+            @foreach($livres as $livre)
+                <li class="item_li3" hidden>
                     <img class="item_image" src="../images/img_couvert_livres/{{$livre->getCategorieId()}}/{{$livre->getIsbnPapier()}}.jpg" alt="">
                     <h3>{{$livre->getTitre()}}</h3>
                     <p>@foreach ($livre->getAuteurAssociee($livre->getId()) as $livreAssocAuteur)
@@ -31,86 +91,10 @@
             @endforeach
         </ul>
         <div class="flecheVisionneuse">
-            <div class="cercleGauche1">
+            <div class="cercleGauche3">
                 <div class="flecheGauche"></div>
             </div>
-            <div class="cercleDroit1">
-                <div class="flecheDroite"></div>
-            </div>
-        </div>
-    </div>
-
-    <h2 class="h2 h2_section2">Ceux qui ont reçu des prix</h2>
-    <div class="prix_recu">
-        <!-- Importer les infos de la base de donnée en mode aléatoire (Titre de livre / Nom auteur / Catégorie / Prix en cad) -->
-
-        <ul>
-            <li class="item_li">
-                <img class="item_image" src="https://placehold.co/210x340" alt="">
-                <h3>La quête d'Albert</h3>
-                <p>Arsenault Isabelle</p>
-                <p>BD jeunesse</p>
-                <p class="prix">18.95$</p>
-            </li>
-            <li class="item_li">
-                <img class="item_image" src="https://placehold.co/210x340" alt="">
-                <h3>Mon coeur Pédale</h3>
-                <p>Leduc Émilie</p>
-                <p>BD jeunesse</p>
-                <p class="prix">27.95$</p>
-            </li>
-            <li class="item_li">
-                <img class="item_image" src="https://placehold.co/210x340" alt="">
-                <h3>Le facteur de l'espace</h3>
-                <p>Perreault Guillaume</p>
-                <p>BD jeunesse</p>
-                <p class="prix">21.95$</p>
-            </li>
-        </ul>
-
-        <div class="flecheVisionneuse">
-            <div class="cercleGauche">
-                <div class="flecheGauche"></div>
-            </div>
-            <div class="cercleDroit">
-                <div class="flecheDroite"></div>
-            </div>
-        </div>
-    </div>
-
-    <h2 class="h2 h2_section3">Des livres recommandés</h2>
-    <div class="livres_recommandes">
-
-        <!-- Importer les infos de la base de donnée en mode aléatoire (Titre de livre / Nom auteur / Catégorie / Prix en cad) -->
-
-        <ul>
-            <li class="item_li">
-                <img class="item_image" src="https://placehold.co/210x340" alt="">
-                <h3>Opération Frankenstein</h3>
-                <p>Solis Fermin</p>
-                <p>Albums jeunesse</p>
-                <p class="prix">9.95$</p>
-            </li>
-            <li class="item_li">
-                <img class="item_image" src="https://placehold.co/210x340" alt="">
-                <h3>Mauvaise herbe</h3>
-                <p>Rassat Thibaut</p>
-                <p>Albums jeunesse</p>
-                <p class="prix">21.95$</p>
-            </li>
-            <li class="item_li">
-                <img class="item_image" src="https://placehold.co/210x340" alt="">
-                <h3>Tom la tondeuse</h3>
-                <p>PA Sophie</p>
-                <p>Albums jeunesse</p>
-                <p class="prix">18.95$</p>
-            </li>
-        </ul>
-        <div class="flecheVisionneuse">
-            <div class="cercleGauche">
-                <div class="flecheGauche"></div>
-            </div>
-            <div class="cercleDroit">
+            <div class="cercleDroit3">
                 <div class="flecheDroite"></div>
             </div>
         </div>
