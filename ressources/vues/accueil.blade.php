d351f5c2fcb50ba61e15440377b0f59cae0b00fb
Justin Carrier <2162774@csfoy.ca>
2023-11-15 08:53:37 -0500

    Retirer message erreur dans accueil

--- ressources/vues/accueil.blade.php
+++ ressources/vues/accueil.blade.php
@@ -18,10 +18,7 @@
     <div class="nouveautes">
         <!-- Importer les infos de la base de donnée en mode aléatoire (Titre de livre / Nom auteur / Catégorie / Prix en cad) -->
         <ul class="itemCacheEnMobile">
-<<<<<<< HEAD
 
-=======
->>>>>>> header--css
             @foreach($livres as $livre)
                 <li class="item_li" hidden>
                     <img class="item_image" src="../images/img_couvert_livres/{{$livre->getCategorieId()}}/{{$livre->getIsbnPapier()}}.jpg" alt="">
