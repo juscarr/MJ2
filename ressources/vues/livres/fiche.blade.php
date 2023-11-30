bf0fda7f6a9471af3e87d4325c12ebc28f5390c2
Jeremi Savard <2111653@csfoy.ca>
2023-11-30 15:28:17 -0500

    corrections pull

--- ressources/vues/livres/fiche.blade.php
+++ ressources/vues/livres/fiche.blade.php
@@ -8,13 +8,22 @@
                 @foreach ($livre->getAuteurAssociee($livre->getId()) as $livreAssocAuteur)
                     {{$livreAssocAuteur->getAuteurAssoc($livreAssocAuteur->getIdAuteur())->getPrenom() . " " . $livreAssocAuteur->getAuteurAssoc($livreAssocAuteur->getIdAuteur())->getNom()}}
                 @endforeach</p></h2>
-        <p><strong>{{$livre->getPrixCan()}} </strong>{{$livre->getPagination()}}</p>
+        <p><strong>{{$livre->getPrixCan()}} </strong>|| {{$livre->getPagination()}} pages</p>
         <p><strong>{{$livre->getAgeMin()}} ans et plus</strong></p>
     </div>
     {{$livre->getId()}}
 
     <div class="article">
+        @php
+            $imagePath = "../images/img_couvert_livres/{$livre->getCategorieId()}/{$livre->getIsbnPapier()}.jpg";
+        @endphp
 
+        @if (file_exists($imagePath))
+            <img class="couvert_livre" src="{{ $imagePath }}" alt="">
+        @else
+        <!-- Image de remplacement à afficher si l'image n'existe pas -->
+            <img class="item_image" src="https://placehold.co/210x340" alt="Image Placeholder">
+        @endif
         <div class="boutons">
             <form action="index.php?controleur=article&action=ajouter&id={{$livre->getId()}}" method="POST">
                 <button class="btn_principal" id="btn_principal" type="submit">Ajouter au panier</button>
